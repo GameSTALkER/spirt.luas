e28133a5fc500fd803b20c3072c034326d598031
GameSTALkER <52292220+GameSTALkER@users.noreply.github.com>
2021-11-10 22:50:43 +0300

    Add files via upload

---
+++ Radial radio (turn on once).lua
@@ -0,0 +1,30 @@
+
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_0_text_1 "I\'am using config made by nGameSTALkER"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_0_text_2 "UKRAINE"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_0_text_3 "993"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_0_text_4 Super Idol xiaorong"; medic; armor; mine')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_0_text_5 Give me AWP please :3"; give weapon_usp_silencer; give weapon_decoy; give item_assaultsuit; give weapon_taser')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_0_text_6 "I HATE ANIME"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_0_text_7 "1000 - 7?"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_0_text_8 "RUSSIA"')
+
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_1_text_1 - Got AWP + P250 + Smoke"; awp; smoke; p250')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_1_text_2 - Got Deagle"; deagle')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_1_text_3 - Got HE"; he')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_1_text_4 - Got Nova"; nova')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_1_text_5 - Got Scout + Deagle"; scout; deagle')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_1_text_6 - Got NEGEV, RUN AWAY B#TCHES!"; negev')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_1_text_7 - Got Fire nade"; molotov')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_1_text_8 - Got FN57"; tec9')
+
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_2_text_1 ~ Drop knife ability is OFF now"; mp_drop_knife_enable 0')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_2_text_2 ~ Bowie Knife spawned"; give weapon_knife_survival_bowie; mp_drop_knife_enable 1; ent_fire weapon_knife addoutput "classname weapon_knifegg"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_2_text_3 ~ Karambit knife spawned"; give weapon_knife_karambit; mp_drop_knife_enable 1; ent_fire weapon_knife addoutput "classname weapon_knifegg"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_2_text_4 ~ Stiletto knife spawned"; give weapon_knife_stiletto; mp_drop_knife_enable 1; ent_fire weapon_knife addoutput "classname weapon_knifegg"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_2_text_5 ~ Ghost Knife spawned"; give weapon_knife_ghost; mp_drop_knife_enable 1; ent_fire weapon_knife addoutput "classname weapon_knifegg"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_2_text_6 ~ Paracord Knife spawned"; give weapon_knife_cord; mp_drop_knife_enable 1; ent_fire weapon_knife addoutput "classname weapon_knifegg"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_2_text_7 ~ Butterfly Knife spawned"; give weapon_knife_butterfly; mp_drop_knife_enable 1; ent_fire weapon_knife addoutput "classname weapon_knifegg"')
+EngineClient.ExecuteClientCmd('cl_radial_radio_tab_2_text_8 ~ CSS knife spawned"; give weapon_knife_css; mp_drop_knife_enable 1; ent_fire weapon_knife addoutput "classname weapon_knifegg"')
+
+EngineClient.ExecuteClientCmd('clear')
+print('Excellent!')
